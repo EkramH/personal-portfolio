58dd4f4732d5f7106a82589f50bdfa7a90d6a481
Ekram Hosen <ekramhosen329@gmail.com>
2022-06-10 14:37:24 +0600

    contact section

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -5,13 +5,16 @@ import Tooth from "../assets/toothcare.png";
 
 const Projects = () => {
   return (
-    <div name="work" className="w-full md:h-screen text-gray-300 bg-[#0a192f]">
+    <div
+      name="projects"
+      className="w-full md:h-screen text-gray-300 bg-[#0a192f]"
+    >
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
         <div className="pb-8">
           <p className="text-4xl font-bold inline border-b-4 text-gray-300 border-pink-600">
-            Work
+            Projects
           </p>
-          <p className="py-6">Check out some of my recent work</p>
+          <p className="py-6">Check out some of my recent projects</p>
         </div>
 
         {/* Container */}
