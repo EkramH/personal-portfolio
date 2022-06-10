ebe1513b24d0a692b9442634efd14eb8d4d20d45
Ekram Hosen <ekramhosen329@gmail.com>
2022-06-10 10:32:21 +0600

    home section

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,25 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a192f]">
-      <h2>home</h2>
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="md:text-2xl text-pink-700">Hi, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-slate-200">
+          Ekram Hosen
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-slate-300">
+          I'm a web developer
+        </h2>
+        <p className="text-slate-300 py-4 max-w-[700px]">
+          I'm a web developer specializing building website with all device
+          responsive. Also, I have experience in REACT, JS and MERN.
+        </p>
+        <div>
+          <button className="text-white font-semibold border-2 rounded-md px-6 py-3 my-2 flex items-center hover:bg-pink-700 duration-300">
+            Resume
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
